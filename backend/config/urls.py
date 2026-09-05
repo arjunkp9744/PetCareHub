b334606ed4a290c33d9d957a2b39fb093328d742
unknown <you@example.com>
2026-09-05 17:58:50 +0530

    Serve media files in production

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -20,8 +20,7 @@ urlpatterns = [
     path( "api/boarding/", include("boarding.urls"),),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
